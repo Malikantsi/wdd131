270e1ac3985f8af1fcb473238432149060d10475
malikantsi <lekhoanalucy@gmail.com>
2025-03-29 22:06:12 +0200

    images changed

--- scripts/places.js
+++ scripts/places.js
@@ -1,6 +1,8 @@
 const lastModified = document.querySelector("#lastModified");
 const year = document.querySelector("#currentyear");
 const today = new Date();
+const temperature = 20;
+const windSpeed = 5;
 
 lastModified.innerHTML = `Last Modified: ${new Intl.DateTimeFormat(
     "en-UK",
@@ -11,8 +13,7 @@ lastModified.innerHTML = `Last Modified: ${new Intl.DateTimeFormat(
 
 year.innerHTML = today.getFullYear();
 
-const temperature = document.querySelector('#temperature');
-const windspeed = document.querySelector('#windspeed');
+
 
 function calculateWindChill(temp, windSpeed) {
     return 13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16);
@@ -22,12 +23,13 @@ function updateWindChill() {
     const windChillElement = document.querySelector('#windChillValue');
     let windChill = 'N/A';
 
-    // Check calculation viability (metric criteria)
+    
     if (temperature <= 10 && windSpeed > 4.8) {
         const calculatedChill = calculateWindChill(temperature, windSpeed);
         windChill = `${calculatedChill.toFixed(1)} °C`;
     }
 
     windChillElement.textContent = windChill;
+    console.log( windChill);
 }
 document.addEventListener('DOMContentLoaded', updateWindChill);
